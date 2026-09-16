0b6ed3a60157eca698b0a9e5f48a750479bf16f9
izzy.png <zineb.azmi.art@gmail.com>
2026-09-16 09:29:36 -0400

    Week2

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Hello, World!");
+// STEP 1: Perform the time-honoured "Hello World!" by compiling the above code
+
+// STEP 2: Create a simple variable setting the data type to string
+
+// STEP 3: Write the value of the variable to the console using WriteLine
+
+// STEP 4: Compile and run the app
+
+// STEP 5: Change the value of the variable set in STEP 2
+
+// STEP 6: Write the value of the variable to the console using the WriteLine method again
+
+// STEP 7: Modify the above two lines that generate the output to make a phrase ("The weather today is " + weatherToday)
+
+// STEP 8: Change STEP 7 so that the line of code uses string interpolation instead of concatenation ($"The weather today is {weatherToday}")
+
+// STEP 9: Write a new line that builds a phrase using two variables
+
+// STEP 10: You can output the number of letters in a string using the Length property
+
+// STEP 11: Create a string variable for a sentence with spaces at the start and end
+
+// STEP 12: Output the above string variable to the console with square brackets around it
+
+// STEP 13: There are other methods for string objects, like Trim() - create a new string variable from the trimmed variable above
+
+// STEP 14: Output the above string variable also between square brackets
+
+// STEP 15: We can also find and replace text in a string - change "windy" to "snowy" in the above variable using the Replace() method
+
+// STEP 16: Output the revised variable to the console
+
+// STEP 17: Output the above sentence, but in ALL CAPS using the ToUpper string method
+
+// STEP 18: We can even search for strings - the Contains() method looks for a string and returns 'true' if it is found, and 'false' if it is not - check to see if the new forecast for Wednesday is "snowy"
+
+// STEP 19: Update the above to look for "rainy" instead
